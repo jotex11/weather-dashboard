c55d827b6b1fde8e4294922b9b4b93872b116bd9
joseph <jomill0723@outlook.com>
2023-08-09 20:12:17 -0400

    tried fixing, but couldn't figure out.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weather-dashboard
+## Task
+My task was to complete the weather dashboard and to show the current weather and the search history. I wasn't really able to find time to get this done, I couldn't find a reasonable time to get a tutor and I was working on the project. I searched up some solutions for my problems, but had no luck. If you could just let me know what I did wrong it would be great
+
+## URLS
+link to github: https://github.com/jotex11/weather-dashboard
+
+link to deployed website: 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,122 +1,112 @@
+// Here is an enhanced and improved version of the 'code_under_test' code snippet:
+
 $(document).ready(function () {
     // Define variables for DOM elements
-    let col2El = $(".col2");
-    let fivDayEl = $("#five-day");
-    let searchHistoryEl = $("#search-history");
+    var col2El = $(".col2");
+    var fivDayEl = $("#five-day");
+    var searchHistoryEl = $("#search-history");
+    let searchHistory = loadHistory();
 
     // Load search history from local storage and create history buttons
     function loadHistory() {
-        let searchHistory = localStorage.getItem('searchHistory');
-        if (searchHistory) {
-            searchHistory = JSON.parse(searchHistory);
-            for (let i = 0; i < searchHistory.length; i++) {
-                createHistoryButton(searchHistory[i]);
+        let searchHistory = localStorage.getItem('search-history');
+        if (!searchHistory) {
+            searchHistory = {
+                searchedCity: [],
+            };
+        } else {
+            for (let i = 0; i < searchHistory.searchedCity.length; i++) {
+                search(searchHistory.searchedCity[i]);
             }
         }
-    }
-
-    // Save city to search history in local storage and create history button
-    function saveHistory(city) {
-        let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-        searchHistory.push(city);
-        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
-        createHistoryButton(city);
+        return searchHistory;
     }
 
     // Create a history button for a city
     function createHistoryButton(city) {
-        let historyBtn = $('<button>')
+        // Create a button element using jQuery
+        const historyBtn = $('<button>')
             .addClass('btn')
             .text(city)
             .on('click', function () {
+                // Remove the current weather and five-day forecast elements
                 $("#current-weather").remove();
                 $("#five-day").empty();
                 $("#five-day-header").remove();
+                // Call the getWeather function with the city as an argument
                 getWeather(city);
             })
             .attr({
                 type: 'button'
             });
 
+        // Append the history button to the search history element
         searchHistoryEl.append(historyBtn);
+    }
+
+    function cityLocation(cityname, state) {
+        let cityURL = 'https://api.openweathermap.org/geo/1.0/direct?q=';
+        if (state) {
+            cityURL += `${cityname}, ${state}, US`;
+        } else {
+            cityURL += cityname;
+        }
+        cityURL += "&appid=" + apiKey;
+
+        fetch(cityURL)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.error('Error fetching city URL', response.status);
+                }
+            })
+            .then((data) => {
+                // Update the city info element with the retrieved weather data
+                const cityInfo = document.getElementById('city-info');
+                cityInfo.innerHTML = `
+                    <p>City: ${data.name}</p>
+                    <p>Temp: ${data.main.temp}</p>
+                `;
+            })
+            .catch((error) => {
+                console.error('Error fetching city URL', error);
+            });
     }
 
     // Get weather data for a city using OpenWeatherMap API
     function getWeather(city) {
-        let apiKey = "9df96985a7bdae925d0041c4da8ecbb2";
-        let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?";
-        let apiCallURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?";
-        let apiCoordinatesURL = weatherURL + 'q=' + city + '&appid=' + apiKey;
-        let currentDay = dayjs();
+        // Define the API key and weather URL
+        var apiKey = '9df96985a7bdae925d0041c4da8ecbb2';
+        var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`;
 
-        fetch(apiCoordinatesURL)
-            .then(function (response) {
+        // Make an API call to fetch weather data
+        fetch(weatherURL)
+            .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
-                    throw new Error('Error fetching weather data:', response.status);
+                    console.error('Error fetching weather data', response.status);
                 }
             })
-            .then(function (data) {
-                let cityLat = data.coord.lat;
-                let cityLong = data.coord.lon;
-                let apiCall = apiCallURL + cityLat + '&lon=' + cityLong + '&appid=' + apiKey + '&units=imperial';
-
-                fetch(apiCall)
-                    .then(function (weatherResponse) {
-                        if (weatherResponse.ok) {
-                            return weatherResponse.json();
-                        } else {
-                            throw new Error('Error fetching weather data:', weatherResponse.status);
-                        }
-                    })
-                    .then(function (weatherData) {
-                        let currWeather = $('<div>')
-                            .attr({
-                                id: 'current-weather'
-                            });
-
-                        let cityNameEl = $('<h2>')
-                            .addClass('weather')
-                            .text(city);
-
-                        let currentDateEl = $('<p>')
-                            .addClass('date')
-                            .text(currentDay.format('MM/DD/YYYY'));
-
-                        let tempEl = $('<p>')
-                            .addClass('temp')
-                            .text('Temperature: ' + weatherData.current.temp + ' °F');
-
-                        let humidityEl = $('<p>')
-                            .addClass('humidity')
-                            .text('Humidity: ' + weatherData.current.humidity + '%');
-
-                        let windSpeedEl = $('<p>')
-                            .addClass('wind')
-                            .text('Wind Speed: ' + weatherData.current.wind_speed + ' MPH');
-
-                        let weatherIcon = $('<img>')
-                            .attr({
-                                src: "picture URL", // Replace with your picture URL
-                                alt: "Weather Icon",
-                            })
-                            .addClass('weather-icon');
-
-                        currWeather.append(cityNameEl, currentDateEl, tempEl, humidityEl, windSpeedEl, weatherIcon);
-                        col2El.append(currWeather);
-                    })
-                    .catch(function (error) {
-                        console.error('Error fetching weather data:', error);
-                    });
+            .then((data) => {
+                // Update the city info element with the retrieved weather data
+                var cityInfo = document.getElementById('city-info');
+                cityInfo.innerHTML = `
+                    <p>City: ${data.name}</p>
+                    <p>Temp: ${data.main.temp}</p>
+                `;
             })
-            .catch(function (error) {
-                console.error('Error fetching weather data:', error);
+            .catch((error) => {
+                console.error('Error fetching weather data', error);
             });
     }
 
-    // start the page
-    loadHistory();
-    getWeather();
+    // Initialize the page
+    $(document).ready(function () {
+        // Load search history
+        loadHistory();
+        // Get weather data for the default city
+        getWeather();
+    });
 });
